4b9fb85f00e375b0e3d7691bd977279075bcae68
eloipereira <eloipereira@gmail.com>
2013-01-02 16:03:47 +0000

    minor changes

--- src/bigactors/BigraphSchdl.scala
+++ src/bigactors/BigraphSchdl.scala
@@ -10,16 +10,16 @@ class BigraphSchdl(brs0 : BRS) extends Actor{
     println("Initial bigraph: " + brs)
      loop {
        react{
-         case x: Query => println("got a obs request " + x + " from "+sender)
-         case x: BRR => {println("got a ctr request " + x + " from "+sender)
+         case x: Query => println("got a obs request " + x + " from "+sender) //TODO - implement query language
+         case x: BRR => {println("got a ctr request " + x + " from "+sender) //TODO - check if host is included on redex and reactum
                          brs.applyRules(List(x),2)
                          println("New bigraph: " + brs)
                         }
-         case x: Node => println("got a mgrt request " + x + " from "+sender)
+         case x: Node => println("got a mgrt request " + x + " from "+sender) //TODO - implement migration
          case x: Message => {println("got a snd request " + x + " from "+sender)
                              if (sender.asInstanceOf[BigActor].host == x.dest.host || sender.asInstanceOf[BigActor].host.getNames.diff(x.dest.host.getNames).isEmpty){
-                                 println("Yap, they're connected!")     //TODO - fix the connection check
-                             } else println("Not connected!")
+                                 println("Yap, they're connected!")
+                             } else System.err.println("BigActors " + sender + " and " + x.dest + " are not connected.")
                             }
        }
      }
