83b1e061c3a02f6a29ba463cf6cf52095a1e7fcc
eloipereira <eloipereira@gmail.com>
2013-04-25 00:40:48 -0700

    Introduce visualization using bigvis

--- src/bigactors/Initializer.scala
+++ src/bigactors/Initializer.scala
@@ -4,7 +4,7 @@ import edu.berkeley.eloi.bigraph.BRS
 
 object Initializer {
   lazy val scheduler = {
-    val brs: BRS = new BRS("/Users/eloipereira/Dropbox/IDEAWorkspace/BigActors/src/examples/simple.bgm",true)
+    val brs: BRS = new BRS("/Users/eloipereira/Dropbox/IDEAWorkspace/BigActors/src/examples/simple.bgm",true,true)
         val scheduler_ = new BigraphSchdl(brs)
         scheduler_.start()
         scheduler_
