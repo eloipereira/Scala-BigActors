b9f0c63337369db229c814b70a10583f031617bf
eloipereira <eloipereira@gmail.com>
2014-04-19 20:48:51 +0100

    bigraph with host stuff

--- src/main/scala/BigraphManagerAPI.scala
+++ src/main/scala/BigraphManagerAPI.scala
@@ -9,3 +9,4 @@ sealed trait BigraphManagerAPI extends Serializable
 case class EXECUTE_BRR(brr: BRR) extends BigraphManagerAPI
 case object BIGRAPH_REQUEST extends BigraphManagerAPI
 case class BIGRAPH_RESPONSE(bigraph: Bigraph) extends BigraphManagerAPI
+case object BIGRAPH_WITH_HOST_REQUEST extends BigraphManagerAPI
